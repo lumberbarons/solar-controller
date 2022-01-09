4e25f1cb69ffad20739a529254eb1b108142d2c7
lumberbarons <jellwood@gmail.com>
2022-01-09 16:54:48 -0500

    Remove unused things.

--- site/src/pages/query.js
+++ site/src/pages/query.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import axios from 'axios';
-import { Box, Grid } from '@material-ui/core';
+import { Box, Grid, Container } from '@material-ui/core';
 import { FormControl, InputLabel, Select, MenuItem, TextField, Button } from '@material-ui/core';
 
 class Query extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {register: '3', address: '0x3100'};
+    this.state = {register: '4', address: '0x3100', resultHex: '', resultDecimal: 0};
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -40,13 +40,13 @@ class Query extends React.Component {
     const address = this.state.address;
 
     return (
+      <Container component="main" maxWidth="xs">
       <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      onSubmit={this.handleSubmit}
-      maxWidth="xs"
-    >
+        component="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={this.handleSubmit}
+      >
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <FormControl fullWidth>
@@ -88,6 +88,7 @@ class Query extends React.Component {
         </Grid>
       </Grid>
     </Box>
+    </Container>
     );
   }
 };
