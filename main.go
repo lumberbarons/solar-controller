1141188b64907db665cd55be48753b522dcc98d5
lumberbarons <jellwood@gmail.com>
2022-10-23 09:43:37 -0400

    Use config file from command line.

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"embed"
+	"flag"
 	"github.com/gin-gonic/gin"
 	"github.com/goburrow/modbus"
-	"github.com/lumberbarons/epever-controller/config"
+	"github.com/lumberbarons/epever-controller/configuration"
 	"github.com/lumberbarons/epever-controller/metrics"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 	"io/fs"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"time"
-	"gopkg.in/yaml.v3"
-	"embed"
 )
 
 //go:embed site/build
 var site embed.FS
+
+var (
+	configFilePath  *string
+	debugMode   *bool
+)
 
 type Config struct {
 	EpeverController EpeverController `yaml:"epeverController"`
@@ -29,15 +34,18 @@ type EpeverController struct {
 }
 
 func init() {
-	if os.Getenv("DEBUG") == "true" {
-		log.SetLevel(log.DebugLevel)
-	}
+	configFilePath = flag.String("config", "", "Config file path")
+	debugMode = flag.Bool("debug", false, "Debug mode")
 }
 
-func loadConfigFile(configFilePath string) Config {
-	configFile, err := ioutil.ReadFile(configFilePath)
+func loadConfigFile() Config {
+	if *configFilePath == "" {
+		log.Fatalf("Must specify config file path")
+	}
+
+	configFile, err := ioutil.ReadFile(*configFilePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to load configuration file: %v", err)
 	}
 
 	config := Config{}
@@ -51,14 +59,16 @@ func loadConfigFile(configFilePath string) Config {
 }
 
 func main() {
-	log.Println("starting epever-controller")
+	log.Info("Starting epever-controller")
 
-	serialPort := os.Getenv("SERIAL_PORT")
-	if serialPort == "" {
-		log.Fatal("serial port not set")
+	flag.Parse()
+
+	if *debugMode {
+		log.SetLevel(log.DebugLevel)
 	}
 
-	handler := modbus.NewRTUClientHandler(serialPort)
+	controllerConfig := loadConfigFile()
+	handler := modbus.NewRTUClientHandler(controllerConfig.EpeverController.SerialPort)
 
 	handler.BaudRate = 115200
 	handler.DataBits = 8
@@ -69,7 +79,7 @@ func main() {
 
 	err := handler.Connect()
 	if err != nil {
-		log.Fatal("failed to connect to serial port")
+		log.Fatalf("Failed to connect to serial port: %v", err)
 	}
 
 	defer handler.Close()
@@ -91,22 +101,26 @@ func main() {
 
 	r.GET("/api/metrics", collector.MetricsGet())
 
-	configurer := config.NewSolarConfigurer(client)
+	configurer := configuration.NewSolarConfigurer(client)
 
-	r.GET("/api/config", configurer.ConfigGet())
-	r.PATCH("/api/config", configurer.ConfigPatch())
+	r.GET("/api/configuration", configurer.ConfigGet())
+	r.PATCH("/api/configuration", configurer.ConfigPatch())
 	r.POST("/api/query", configurer.QueryPost())
 
 	r.StaticFS("/", staticFS())
 
-	r.Run()
+	err = r.Run()
+
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
 
 func staticFS() http.FileSystem {
 	sub, err := fs.Sub(site, "site/build")
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to load static site: %v", err)
 	}
 
 	return http.FS(sub)
